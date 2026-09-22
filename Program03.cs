ba318f2ddd2faeda390e65d44a3772faf9abf600
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-22 16:19:02 +0530

    indexoutofrangeexception

---
+++ Program03.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSIGNMENT_05
+{
+    internal class Program03
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+
+            Console.WriteLine("Enter 5 elements:");
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            try
+            {
+                Console.WriteLine("Enter the index of the element you want to access (0-4):");
+                int index = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Element at index {index} is: {arr[index]}");
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error: Index is out of range. Please enter an index between 0 and 4.");
+            }
+        }
+    }
+}
