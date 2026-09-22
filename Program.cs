5a643c5a88818f717acf5040af404d72e1e4c345
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-22 15:58:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace ASSIGNMENT_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter another number:");
+                int b = int.Parse(Console.ReadLine());
+
+                int result = a / b;
+                Console.WriteLine($"The result of {a} divided by {b} is: {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: You cannot divide by zero.");
+            }
+            
+        }
+    }
+}
