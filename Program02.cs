2365aae6418f7e0c733ebb2ba420d2e59c66ce81
Dolesh kumar <hanubhai0007@gmail.com>
2026-09-22 16:02:58 +0530

    Format Exception

---
+++ Program02.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASSIGNMENT_05
+{
+    internal class Program02
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter an integer: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine($"You entered: {number}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: Please enter a valid integer.");
+            }
+        }
+    }
+}
